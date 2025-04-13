54e0c2aaacbd40ff807d1eb3b58f4776bc32c757
LemonAttn <1837669410@qq.com>
2025-04-13 21:53:40 +0800

    update vit and add SigLip

--- mini_transformer/model.py
+++ mini_transformer/model.py
@@ -3,17 +3,8 @@ import torch.nn as nn
 from einops import repeat
 from einops.layers.torch import Rearrange
 
-from .layer import Block, RMSNorm, precompute_rope_emb
+from .layer import Block, RMSNorm, precompute_rope_emb, Extract, Transformer
 from .utils import check_tuple
-
-
-class ExtractCls(nn.Module):
-
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, x: torch.Tensor):
-        return x[:, 0]
 
 
 class VIT(nn.Module):
@@ -21,21 +12,23 @@ class VIT(nn.Module):
     def __init__(self,
                  # common
                  img_shape: int = 224,
-                 patch_size: int = 32,
+                 patch_size: int = 16,
                  channel: int = 3,
                  # mla
                  dim: int = 512,
-                 inter_dim: int = 1344,
                  n_head: int = 8,
                  q_lora_rank: int = 0,
                  kv_lora_rank: int = 128,
                  qk_nope_head_dim: int = 32,
                  qk_rope_head_dim: int = 16,
                  v_head_dim: int = 32,
+                 # mlp:
+                 inter_dim: int = 1344,
                  # Transformer
                  n_layer: int = 8,
                  # out
-                 num_classes: int = 1000
+                 out_method: str = 'img',
+                 num_classes: int = None
                  ):
         super().__init__()
 
@@ -58,10 +51,10 @@ class VIT(nn.Module):
             for _ in range(n_layer)
         ])
         self.to_out = nn.Sequential(
-            ExtractCls(),
+            Extract('cls'),
             nn.RMSNorm(dim),
             nn.Linear(dim, self.num_classes)
-        ) if num_classes is not None else ExtractCls()
+        ) if num_classes is not None else Extract(out_method)
 
         self.register_buffer('rope_emb',
                              precompute_rope_emb(dim = qk_rope_head_dim, seq_len = self.seq_len),
@@ -80,3 +73,67 @@ class VIT(nn.Module):
             x, _ = layer(x, start_pos = 0, rope_emb = self.rope_emb)
         out = self.to_out(x)
         return out
+
+
+class SigLip(nn.Module):
+
+    def __init__(self,
+                 # img
+                 img_shape: int = 224,
+                 patch_size: int = 16,
+                 channel: int = 3,
+                 # text
+                 vocab_size: int = 6400,
+                 max_seq_len: int = 512 * 8,
+                 # mla
+                 dim: int = 512,
+                 n_head: int = 8,
+                 q_lora_rank: int = 0,
+                 kv_lora_rank: int = 128,
+                 qk_nope_head_dim: int = 32,
+                 qk_rope_head_dim: int = 16,
+                 v_head_dim: int = 32,
+                 # mlp:
+                 inter_dim: int = 1344,
+                 # Transformer
+                 n_layer: int = 8,
+                 ):
+        super().__init__()
+
+        self.img_encoder = VIT(
+            img_shape = img_shape,
+            patch_size = patch_size,
+            channel = channel,
+            dim = dim,
+            n_head = n_head,
+            q_lora_rank = q_lora_rank,
+            kv_lora_rank = kv_lora_rank,
+            qk_nope_head_dim = qk_nope_head_dim,
+            qk_rope_head_dim = qk_rope_head_dim,
+            v_head_dim = v_head_dim,
+            inter_dim = inter_dim,
+            n_layer = n_layer,
+            out_method = 'cls',
+        )
+        self.text_encoder = Transformer(
+            vocab_size = vocab_size,
+            dim = dim,
+            n_head = n_head,
+            q_lora_rank = q_lora_rank,
+            kv_lora_rank = kv_lora_rank,
+            qk_nope_head_dim = qk_nope_head_dim,
+            qk_rope_head_dim = qk_rope_head_dim,
+            v_head_dim = v_head_dim,
+            inter_dim = inter_dim,
+            n_layer = n_layer,
+            attn_name = 'mla',
+            mlp_name = 'mlp'
+        )
+        self.text_encoder.to_out = Extract(method = 'post_mean')
+
+    def forward(self, img: torch.Tensor, text: torch.Tensor):
+        # img:[b, c, h, w]
+        # text:[b, n]
+        img = self.img_encoder(img)
+        text, _ = self.text_encoder(text, start_pos = 0)
+        return img, text
